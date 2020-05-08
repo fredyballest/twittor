a8684121c64074a03acc8bb289ec4ea1644696d7
Fredy Ballesteros <fredy.ballesteros@gmail.com>
2020-05-07 20:53:50 -0500

    Configuramos el service worker

--- js/app.js
+++ js/app.js
@@ -1,7 +1,20 @@
+var url = window.location.href;
+var swLocation = '/twittor/sw.js';
 
-if ( navigator.serviceWorker ){
-    navigator.serviceWorker.register('/sw.js')
+
+if ( navigator.serviceWorker ) {
+
+
+    if ( url.includes('localhost') ) {
+        swLocation = '/sw.js';
+    }
+
+
+    navigator.serviceWorker.register( swLocation );
 }
+
+
+
 
 
 
