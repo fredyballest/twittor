e754ea47982064aacca97a969d520ee312ecf3e1
Fredy Ballesteros <fredy.ballesteros@gmail.com>
2020-05-07 20:38:31 -0500

    Primer Envio

---
+++ js/sw-utils.js
@@ -0,0 +1,23 @@
+
+
+// Guardar  en el cache dinamico
+function actualizaCacheDinamico( dynamicCache, req, res ) {
+
+
+    if ( res.ok ) {
+
+        return caches.open( dynamicCache ).then( cache => {
+
+            cache.put( req, res.clone() );
+            
+            return res.clone();
+
+        });
+
+    } else {
+        return res;
+    }
+
+
+
+}
